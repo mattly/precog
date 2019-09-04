d318cdac7fb792ff087df22fa13d74bd852151b5
Matthew Lyon <matthew@lyonheart.us>
2019-09-04 09:37:26 -0700

    boom

---
+++ readme.md
@@ -0,0 +1,3 @@
+# CLJS/Preact Base
+
+This is an experiment with using CLJS and [Preact](https://preactjs.com) together. There's not really anything here yet.
---
+++ src/dv/main.cljs
@@ -0,0 +1,39 @@
+(ns dv.main
+  (:require
+   [cljs-bean.core :refer [bean ->clj ->js]]
+   ["preact" :as preact]))
+
+(defn ele [node]
+  (cond (string? node) node
+        (list? node) (->js (map ele node))
+
+        (vector? node)
+        (let [[c & pc] node
+              component (if (keyword? c) (name c) c)
+              [props childs] (if (map? (first pc))
+                               [(->js (first pc)) (rest pc)]
+                               [#js {} pc])
+              children (map ele childs)]
+          (apply preact/h component props children))))
+
+(defn app [props]
+  (ele [:div [:div "Hello"] [:div "World?"]]))
+
+(def app-ele (js/document.getElementById "app"))
+
+(defn render []
+  (js/console.log ::render)
+  (loop []
+    (if (.hasChildNodes app-ele)
+      (do (.removeChild app-ele (.-firstChild app-ele))
+        (recur))
+      :done))
+  (preact/render (ele [app]) app-ele))
+
+(defn main! []
+  (js/console.log ::main!)
+  (render))
+
+(defn reload! []
+  (js/console.log ::reload!)
+  (render))

9564473358c3230b3499d94c1f0b92fc02b4fda2
Matthew Lyon <matthew@lyonheart.us>
2019-09-05 20:23:03 -0700

    towards state

--- src/dv/main.cljs
+++ src/dv/main.cljs
@@ -1,39 +1,56 @@
 (ns dv.main
   (:require
-   [cljs-bean.core :refer [bean ->clj ->js]]
-   ["preact" :as preact]))
+   ["preact/hooks" :as hooks]
+   [dv.view :as view :refer-macros [defc]]))
 
-(defn ele [node]
-  (cond (string? node) node
-        (list? node) (->js (map ele node))
+(defn use-lens [*a f]
+  (let [[value update-value] (hooks/useState (f @*a))]
+    (hooks/useEffect
+     (fn [_]
+       (let [k (gensym "useLens")]
+         (add-watch *a k
+                    (fn update-lens-hook [_ _ _ new-state]
+                      (update-value (f new-state))))
+         (fn [] (remove-watch *a k)))))
+    value))
 
-        (vector? node)
-        (let [[c & pc] node
-              component (if (keyword? c) (name c) c)
-              [props childs] (if (map? (first pc))
-                               [(->js (first pc)) (rest pc)]
-                               [#js {} pc])
-              children (map ele childs)]
-          (apply preact/h component props children))))
 
-(defn app [props]
-  (ele [:div 
-        [:div "Hello"] 
-        [:div "World?"] 
-        (list 
-         [:div {:key "one"} "hello"]
-         [:div {:key "two"} "world"])]))
+(defc dtdd [{:keys [dt dd]}]
+  [:<> [:dt dt] [:dd dd]])
+
+(defc app [{:keys [state]}]
+  (let [c (use-lens state #(get % :count 0))
+        input (use-lens state #(get % :input ""))]
+    (js/console.log input)
+    [:div
+     [:div "Hello"]
+     [:div "World!!"]
+     [:<> [:div "one"] [:div "two"]]
+     [:dl
+      [dtdd {:dt "term" :dd "definition"}]
+      [dtdd {:dt "term2" :dd "definiton2"}]]
+     [:div
+      [:div
+       "clicked " c " times"]
+      [:div
+       [:button {:onClick (fn [_] (swap! state update :count inc))} "increment"]
+       [:button {:onClick (fn [_] (swap! state update :count dec))} "decrement"]]]
+     [:input {:type "text" :value input :onInput (fn [e] (swap! state assoc :input (.. e -target -value)))}]
+     [:ul
+      (for [x (range 10)]
+        [:li x])]
+     [:<>
+      [:div {:key "one"} "hello"]
+      [:div {:key "two"} "world"]]]))
 
 (def app-ele (js/document.getElementById "app"))
 
+(defonce state (atom {}))
+
 (defn render []
   (js/console.log ::render)
-  (loop []
-    (if (.hasChildNodes app-ele)
-      (do (.removeChild app-ele (.-firstChild app-ele))
-        (recur))
-      :done))
-  (preact/render (ele [app]) app-ele))
+  (view/render [app {:state state}] app-ele)
+  (js/console.log ::render-done))
 
 (defn main! []
   (js/console.log ::main!)
---
+++ src/dv/view.cljc
@@ -0,0 +1,50 @@
+(ns dv.view
+  #?(:cljs (:require-macros [dv.view]))
+  #?(:cljs (:require 
+            [cljs-bean.core :refer [bean ->js ->clj]]
+            ["preact" :as preact])))
+
+#?(:cljs
+   (defn compile-template [form]
+     (cond
+       (vector? form)
+       (let [[c & pc] form
+             component (cond (contains? #{:< :<>} c) preact/Fragment
+                             (keyword? c) (name c)
+                             :else c)
+             [props children] (if (map? (first pc))
+                                [(first pc) (rest pc)]
+                                [{} pc])]
+         (js-obj "constructor" js/undefined
+                 "type" component
+                 "ref" (or (:ref props) js/undefined)
+                 "key" (or (:key props) js/undefined)
+                 "props" (->js (merge (bean)
+                                      (dissoc props :key :ref)
+                                      {:children (to-array (map compile-template children))}))))
+       
+       (coll? form)
+       (apply array (doall (map compile-template form)))
+       
+       :else form)))
+
+#?(:cljs
+   (defn from-js [props]
+     (->clj (bean props))))
+
+#?(:cljs
+   (defn render [tmpl dom-node]
+     (loop []
+       (if (.hasChildNodes dom-node)
+         (do (.removeChild dom-node (.-firstChild dom-node))
+             (recur))
+         :done))
+     (preact/render (compile-template tmpl) dom-node)))
+
+#?(:clj
+   (defmacro defc [cname binds & forms]
+     (let [bind (or (first binds) '_)]
+       `(defn ~cname [props#]
+          (let [~bind (from-js props#)]
+            ~@(butlast forms)
+            (compile-template ~(last forms)))))))

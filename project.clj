e08f32b499362dc0d8f11f76ec22c02960b7c100
Matthew Lyon <matthew@lyonheart.us>
2019-10-01 22:03:18 -0700

    more tweaks

--- project.clj
+++ project.clj
@@ -1,6 +1,11 @@
 (defproject mattly/precog "0.0.1"
-            :description "A thin wrapper around preact"
-            :url "https://github.com/mattly/precog"
-            :license {:name "MIT"
-                      :url  "https://opensource.org/licenses/MIT"}
-            :source-paths ["src"])
+  :description "A thin wrapper around preact"
+  :url "https://github.com/mattly/precog"
+  :license {:name "MIT"
+            :url  "https://opensource.org/licenses/MIT"}
+  :source-paths ["src"]
+
+  ; Forgive me Phil, for I have sinned
+  :repositories
+  {"clojars" {:url           "https://clojars.org/repo"
+              :sign-releases false}})

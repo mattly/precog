ee7e4253a34612bee118495598ec8b924cd51f32
Matthew Lyon <matthew@lyonheart.us>
2019-10-08 20:46:39 -0700

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mattly/precog "0.0.1"
+(defproject mattly/precog "0.0.2"
   :description "A thin wrapper around preact"
   :url "https://github.com/mattly/precog"
   :license {:name "MIT"
